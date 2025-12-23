b6041a32929dfce33d6a360e9448be604199427b
abhishek-it <abhishekgiri6729@gmail.com>
2025-12-23 23:48:01 +0530

    23 DEC

---
+++ setTimeout.js
@@ -0,0 +1,7 @@
+// setInterval(() => {
+//   const time = new Date().toLocaleTimeString();
+//   console.log(time);
+// }, 1000);
+setTimeout(() => {
+  console.log("This text is added after 4 seconds");
+}, 4000);
